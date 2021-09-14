2042f86d7246de7ab5de29e5fe408e10ffc54d80
Douglas Oliveira <douglasoliveira163425@gmail.com>
2021-09-14 11:55:49 -0400

    ajustado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  //baseURL: 'http://localhost:3333'
+  baseURL: 'http://prestservbackend-env.eba-tyu39hpm.us-east-1.elasticbeanstalk.com'
 });
 
 export {api};
